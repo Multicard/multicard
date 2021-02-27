b7673a419b8701858fa314669ddb50c68ef315c4
frank.fankhauser@students.bfh.ch <frank.fankhauser@students.bfh.ch>
2021-02-27 18:09:52 +0100

    websocket for specific user implemented

--- multicard-backend/src/main/java/ch/cas/html5/multicardgame/messaging/WebSocketController.java
+++ multicard-backend/src/main/java/ch/cas/html5/multicardgame/messaging/WebSocketController.java
@@ -1,16 +1,26 @@
 package ch.cas.html5.multicardgame.messaging;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
 public class WebSocketController {
-    @MessageMapping("/ws-endpoint")
-    @SendTo("/topic/")
-    public String greeting(Message message) throws Exception {
-        return "Hello, " + message.getText();
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    public void sendToUser(String playgroundId, String userId, Message msg) {
+        String dest = "/queue/" + playgroundId + "/" + userId;
+        simpMessagingTemplate.convertAndSend(dest, msg);
+        System.out.println("Message sent to: " + dest);
+    }
+
+    @MessageMapping("/{playgroundId}/{userId}")
+    public void handleMessage(@DestinationVariable String playgroundId, @DestinationVariable String userId, String message) {
+        System.out.println("Message received: " + message + "from: " + playgroundId + "/" + userId);
     }
 
 }
