c4bb8b6a601904c2e5cfa63cb8615946cce77612
tinul <martin.lerch@sunrise.ch>
2021-03-11 15:00:20 +0100

    json Annotation für Subtypen hinzugefügt

--- multicard-api/src/main/java/ch/cas/html5/multicardgame/messages/GameMessage.java
+++ multicard-api/src/main/java/ch/cas/html5/multicardgame/messages/GameMessage.java
@@ -1,7 +1,15 @@
 package ch.cas.html5.multicardgame.messages;
 
 import ch.cas.html5.multicardgame.enums.Action;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "messageName")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = GameStateMessage.class, name = "GameStateMessage"),
+        @JsonSubTypes.Type(value = PlayedCardMessage.class, name = "PlayedCardMessage"),
+        @JsonSubTypes.Type(value = RevertLastPlayerActionMessage.class, name = "RevertLastPlayerActionMessage")
+})
 public class GameMessage {
     private Action command;
 
