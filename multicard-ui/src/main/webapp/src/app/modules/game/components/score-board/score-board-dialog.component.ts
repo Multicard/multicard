3fcb4154798d992688e37de2fe295bd9142cfe00
tinul <martin.lerch@sunrise.ch>
2021-03-29 23:20:19 +0200

    neue Komponente ScoreBoardDialogComponent added

---
+++ multicard-ui/src/main/webapp/src/app/modules/game/components/score-board/score-board-dialog.component.ts
@@ -0,0 +1,33 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {GameDTO, ScoreDTO} from '../../../../../app-gen/generated-model';
+
+@Component({
+  selector: 'mc-score-board-dialog',
+  templateUrl: './score-board-dialog.component.html',
+  styleUrls: ['./score-board-dialog.component.scss']
+})
+export class ScoreBoardDialogComponent implements OnInit {
+
+  game: GameDTO;
+  scores: ScoreDTO[] = [];
+  total: number[] = [];
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) private data: GameDTO,
+  ) {
+    this.game = data;
+  }
+
+  ngOnInit(): void {
+    this.game.scores?.forEach(roundScore => {
+      this.scores.push({
+        id: '',
+        round: roundScore.round,
+        playerScores: [{playerId: '', score: roundScore.playerScores[0].score + roundScore.playerScores[2].score},
+          {playerId: '', score: roundScore.playerScores[1].score + roundScore.playerScores[3].score}]
+      });
+    });
+  }
+
+}
